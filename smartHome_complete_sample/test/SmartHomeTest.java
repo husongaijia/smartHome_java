8f87379689340f31f9cbcc0005233ae1c81d9c74
戸松 愛佳 <tomatsu.aika@jp.fujitsu.com>
2019-09-30 23:07:51 +0900

    testでstubとspyを作成する部分をbeforeメソッドに移動

--- smartHome_complete_sample/test/SmartHomeTest.java
+++ smartHome_complete_sample/test/SmartHomeTest.java
@@ -1,10 +1,17 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SmartHomeTest {
-    private LightSwitchStub lightSwitchStub = new LightSwitchStub();
-    private BulbSpy bulbSpy = new BulbSpy();
+    private LightSwitchStub lightSwitchStub;
+    private BulbSpy bulbSpy;
+
+    @Before
+    public void setUp() throws Exception {
+        lightSwitchStub = new LightSwitchStub();
+        bulbSpy = new BulbSpy();
+    }
 
     @Test
     public void スイッチがONの時バルブがつく() {
